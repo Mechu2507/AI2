43bd6a33b6fa9530829b38b988f97bb3ea467b2b
MarcinSteciuk <steciukmarci@gmail.com>
2024-01-30 20:42:44 +0100

    Update ProtectedRoute.js

--- frontend/src/ProtectedRoute.js
+++ frontend/src/ProtectedRoute.js
@@ -1,28 +1,28 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
-import axios from "react-app-store-badge";
+import axios from "axios"; // Make sure this is correctly imported from 'axios' and not 'react-app-store-badge'
 
 const ProtectedRoute = ({ children, allowedRoles }) => {
-    const [user_id, setUser_id] = useState('');
+    const [userRole, setUserRole] = useState(null);
 
     useEffect(() => {
-        axios
-            .get(`http://127.0.0.1:8000/api/getUserDetails`, {
-                headers: {
-                    Authorization: `Bearer ${localStorage.getItem("token")}`,
-                },
-            })
-            .then((response) => {
-                if (response.data && response.data.user) {
-                    setUser_id(response.data.user.id);
-                }
-            })
-            .catch((e) => {
-                console.error(e);
-            });
+        axios.get(`http://127.0.0.1:8000/api/getUserDetails`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        })
+        .then((response) => {
+            if (response.data && response.data.user) {
+                // Assuming 'role' or 'role_id' is the attribute for user's role
+                setUserRole(response.data.user.role || response.data.user.role_id);
+            }
+        })
+        .catch((e) => {
+            console.error(e);
+        });
     }, []);
 
-    return allowedRoles.includes(user_id) ? children : <Navigate to="/Unauthorized" />;
+    return allowedRoles.includes(userRole) ? children : <Navigate to="/Unauthorized" />;
 };
 
 export default ProtectedRoute;
